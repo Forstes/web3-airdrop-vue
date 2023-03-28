6d44591ffa307eaf6d83f85366453b681b178e92
Forstes <b.rodion70@mail.ru>
2023-03-28 23:08:01 +0600

    ghp conf

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE),
+  history: createWebHistory("/web3-airdrop-vue/"),
   routes,
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE,
+  base: "/web3-airdrop-vue/",
   plugins: [vue()],
   resolve: {
     alias: {
